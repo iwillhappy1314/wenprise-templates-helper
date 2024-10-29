1ff4cc3b023d0357fc23c571016cf9ceb2272f43
Amos Lee <iwillhappy1314@gmail.com>
2024-10-29 09:18:31 +0800

    Bugifx

--- src/TemplateHelper.php
+++ src/TemplateHelper.php
@@ -180,9 +180,9 @@ class TemplateHelper
         // Get default template/.
         if ( ! $template) {
             if (empty($cs_template)) {
-                $template = $this->locate_default_template($template_name, [$default_paths]);
+                $template = $this->locate_default_template($template_name, (array)$default_paths);
             } else {
-                $template = $this->locate_default_template($cs_template, [$default_paths]);
+                $template = $this->locate_default_template($cs_template, (array)$default_paths);
             }
         }
 
@@ -202,7 +202,7 @@ class TemplateHelper
     function locate_default_template($template, $paths)
     {
         foreach ($paths as $path) {
-            $template_path = $path . $template;
+            $template_path = $path . '/' . trim('/',$template);
 
             if (file_exists($template_path)) {
                 return $template_path;
